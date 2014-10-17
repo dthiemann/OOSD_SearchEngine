91396466ae35293dae3ecdece1af8ac91346ad12
TomWerner <tomwer3@gmail.com>
2014-10-17 15:07:38 -0500

    Finished field and field test
    Simple key-value class field is done along with a test that checks that
    the constructor and getters work as expected

--- part1/SearchEngine/src/edu/uiowa/cs2820/engine/Field.java
+++ part1/SearchEngine/src/edu/uiowa/cs2820/engine/Field.java
@@ -2,5 +2,22 @@ package edu.uiowa.cs2820.engine;
 
 public class Field
 {
+    private String fieldName;
+    private String value;
 
+    public Field(String fieldName, String value)
+    {
+        this.fieldName = fieldName;
+        this.value = value;
+    }
+
+    public String getFieldName()
+    {
+        return fieldName;
+    }
+
+    public String getValue()
+    {
+        return value;
+    }
 }
--- part1/SearchEngine/src/edu/uiowa/cs2820/engine/tests/FieldTest.java
+++ part1/SearchEngine/src/edu/uiowa/cs2820/engine/tests/FieldTest.java
@@ -1,22 +1,21 @@
 package edu.uiowa.cs2820.engine.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
+
+import edu.uiowa.cs2820.engine.Field;
 
 public class FieldTest
 {
-
-    @Before
-    public void setUp() throws Exception
-    {
-    }
-
     @Test
-    public void test()
+    public void assert_that_field_constructor_and_getters_work()
     {
-        fail("Not yet implemented");
-    }
-
+        String field = "" + Math.random();
+        String value = "" + Math.random();
+        Field testField = new Field(field, value);
+        assertEquals(testField.getFieldName(), field);
+        assertEquals(testField.getValue(), value);
+        
+    }    
 }
