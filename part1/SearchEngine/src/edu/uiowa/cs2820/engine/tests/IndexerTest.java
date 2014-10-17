62be6332e7a3f27f4869dae5b63b83e0d27a8171
acc4tb <wenbin-lu@uiowa.edu>
2014-10-17 16:16:57 -0500

    Initial commit for Indexer

---
+++ part1/SearchEngine/src/edu/uiowa/cs2820/engine/tests/IndexerTest.java
@@ -0,0 +1,21 @@
+package edu.uiowa.cs2820.engine.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class IndexerTest {
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+
+}
