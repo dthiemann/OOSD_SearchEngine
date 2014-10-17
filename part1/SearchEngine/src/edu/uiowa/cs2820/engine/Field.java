c440dce7cd803de992150c24396f2d9959d0afbc
TomWerner <tomwer3@gmail.com>
2014-10-17 12:21:04 -0500

    Initial commit of eclipse project

---
+++ part1/SearchEngine/src/edu/uiowa/cs2820/engine/Field.java
@@ -0,0 +1,6 @@
+package edu.uiowa.cs2820.engine;
+
+public class Field
+{
+
+}
---
+++ part1/SearchEngine/src/edu/uiowa/cs2820/engine/tests/FieldTest.java
@@ -0,0 +1,22 @@
+package edu.uiowa.cs2820.engine.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FieldTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+
+}
