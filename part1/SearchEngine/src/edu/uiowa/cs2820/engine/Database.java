a489d1de5e7b0a74c56a7a394166d994a518370e
Dylan Thiemann <dylanthiemann@uiowa.edu>
2014-10-18 16:14:59 -0500

    Added basic functionality and a simple test case for the Database class

--- part1/SearchEngine/src/edu/uiowa/cs2820/engine/Database.java
+++ part1/SearchEngine/src/edu/uiowa/cs2820/engine/Database.java
@@ -1,5 +1,30 @@
 package edu.uiowa.cs2820.engine;
 
+import java.util.HashMap;
+
 public class Database {
 
+	private HashMap<String, String> map;
+	private int count;
+	
+	public Database() {
+		map = new HashMap<String, String>();
+		this.count = 0;
+	}
+	
+	public String getValue(String key) {
+			return map.get(key);
+	}
+	
+	public void setValueforKey(String key, String value) {
+		if (key != null) { 
+			map.put(key, value);
+			count++;
+		}
+	}
+	
+	public int getDatabaseSize() {
+		return count;
+	}
+	
 }
--- part1/SearchEngine/src/edu/uiowa/cs2820/engine/tests/DatabaseTest.java
+++ part1/SearchEngine/src/edu/uiowa/cs2820/engine/tests/DatabaseTest.java
@@ -1,6 +1,7 @@
 package edu.uiowa.cs2820.engine.tests;
 
 import static org.junit.Assert.*;
+import edu.uiowa.cs2820.engine.Database;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,11 +12,19 @@ public class DatabaseTest {
     public void setUp() throws Exception
     {
     }
-
+    
     @Test
-    public void test()
-    {
-        fail("Not yet implemented");
-    }
+    public void testKeyValueSetAndGet() {
+    	String key1 = "this is my key";
+    	String value1 = "this is my value";
+    	String key2 = "key2";
+    	String value2 = "value2";
+    	
+    	Database myDB = new Database();
+    	myDB.setValueforKey(key1, value1);
+    	myDB.setValueforKey(key2, value2);
 
+    	assertEquals(myDB.getValue(key1), value1);
+    	assertEquals(myDB.getValue(key2), value2);
+    }
 }
