525c84e3612446dcc277ce1eefd108008b0a5770
acc4tb <wenbin-lu@uiowa.edu>
2014-10-21 00:20:18 -0500

    rewrite Indexer

--- part1/SearchEngine/src/edu/uiowa/cs2820/engine/Indexer.java
+++ part1/SearchEngine/src/edu/uiowa/cs2820/engine/Indexer.java
@@ -1,49 +1,27 @@
 package edu.uiowa.cs2820.engine;
 
-import java.util.ArrayList;
-
 import edu.uiowa.cs2820.engine.Field;
 import edu.uiowa.cs2820.engine.Database;
 
 public class Indexer {
 
     private String identifier;
-    private String fieldName;
-    private ArrayList<String> valueList;
+    private Database db;
+    private boolean closed;
 
-    public Indexer(String identifier) {
+    public Indexer(Database db, String identifier) {
         this.identifier = identifier;
-        this.fieldName = null;
+        this.db = db;
     }
 
     public void add(Field f) {
-        // I feel like this should be something like
-        //      database.add(field, identifier)
-        // also, the field name does not need to be the same
-        // throughout the indexer - Tom
-        
-        String F = f.getFieldName();
-
-        if (fieldName == null) {
-            // it's the 1st field coming in, initialize fieldName
-            fieldName = F;
-            valueList.add(f.getValue());
-
-        } else if (fieldName == F) {
-            // 2nd and future coming fields should match the existing fieldName
-            valueList.add(f.getValue());
-
-        } else {
-            // fieldName does not match, throw exception
-            throw new Exception("Field name does not match");
-        }
+        if (!closed)
+            db.store(f, identifier);
 
     }
 
     public void close() {
-        // I may check database size here.
-        Database D = new Database();
-        D.store(identifier, fieldName, valueList);
+        closed = true;
     }
 
 }
