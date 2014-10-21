1236f90c2ee998499bc2cbd51dfea73e58d62bd8
TomWerner <tomwer3@gmail.com>
2014-10-21 09:06:00 -0500

    Added constructor for fieldsearch
    added the constructor so the code has less errors.

--- part1/SearchEngine/src/edu/uiowa/cs2820/engine/FieldSearch.java
+++ part1/SearchEngine/src/edu/uiowa/cs2820/engine/FieldSearch.java
@@ -3,6 +3,11 @@ package edu.uiowa.cs2820.engine;
 public class FieldSearch
 {
 
+    public FieldSearch(Database database)
+    {
+        // TODO Auto-generated constructor stub
+    }
+
     public String[] findEquals(Field search)
     {
         // TODO Auto-generated method stub
