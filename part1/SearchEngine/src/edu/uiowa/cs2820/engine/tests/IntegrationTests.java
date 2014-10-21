a168fe5d75c91abf7424130cf583121abc32b782
TomWerner <tomwer3@gmail.com>
2014-10-20 21:15:44 -0500

    Added more end to end tests and field
    Added some integration tests that actually go end to end.
    When all the pieces are in place they should pass. I did quick versions
    of indexer, database, and fieldSearch, and using those all the tests I
    had passed, but I won't commit those.
    Field was updated so that it can be used as a key in a hash table.

--- part1/SearchEngine/src/edu/uiowa/cs2820/engine/tests/IntegrationTests.java
+++ part1/SearchEngine/src/edu/uiowa/cs2820/engine/tests/IntegrationTests.java
@@ -1,6 +1,12 @@
 package edu.uiowa.cs2820.engine.tests;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
 import org.junit.Test;
 
@@ -35,6 +41,7 @@ public class IntegrationTests
         // Close the indexer
         indexer.close();
     }
+
     private void setupClassesIndex(Database database)
     {
         // This comes from the UserDemo.java file.
@@ -42,22 +49,26 @@ public class IntegrationTests
         // method as seen in UserDemo.java are operational in Indexer.java
         String fieldName = "class";
         String[] values = { "OOP", "CSII", "Algorithms", "Computer Org" };
-        String identifier = "classes.txt";
+        String identifier1 = "classes.txt";
+        String identifier2 = "classes2.txt";
 
         // The indexer seems to take a series of fields. They don't all have
         // to have the same fieldName. All being in the same indexer just means
         // they come from the same identifier
-        Indexer indexer = new Indexer(database, identifier);
+        Indexer indexer1 = new Indexer(database, identifier1);
+        Indexer indexer2 = new Indexer(database, identifier2);
 
         for (String value : values)
         {
             // Create the field for all the values and add them to the indexer
             Field field = new Field(fieldName, value);
-            indexer.add(field);
+            indexer1.add(field);
+            indexer2.add(field);
         }
 
         // Close the indexer
-        indexer.close();
+        indexer1.close();
+        indexer2.close();
     }
 
     @Test
@@ -67,12 +78,11 @@ public class IntegrationTests
         setupNamesIndex(database);
         setupClassesIndex(database);
 
-        FieldSearch search = new FieldSearch();
+        FieldSearch search = new FieldSearch(database);
         ArrayList<Field> searches = new ArrayList<Field>();
         searches.add(new Field("name", "Tom"));
         searches.add(new Field("class", "OOP"));
-        
-        
+
         for (Field f : searches)
         {
             System.out.println("Looking for field:" + f.getFieldName() + " with value:" + f.getValue());
@@ -84,4 +94,68 @@ public class IntegrationTests
         }
     }
 
+    /*
+     * This test checks that we can index data, it gets saved to the database, and that
+     * a raw query on the database returns the appropriate data. This does not use FieldSearch yet.
+     */
+    @Test
+    public void test_end_to_end() throws FileNotFoundException
+    {
+        // Create our database
+        Database database = new Database();
+        
+        // Set up our data
+        String filename = "test_data.txt";
+        String[] lines = {
+                "names Tom Dylan Wenbin Nicholas",
+                "days Monday Tuesday Wednesday",
+                "classes OOP Algorithms ComputerOrg",
+                "letters a b c d e f g",
+        };
+        
+        // Load our data
+        int lineNum = 0;
+        for (String line : lines)
+        {
+            Indexer indexer = new Indexer(database, filename + " - " + lineNum);
+            String[] lineData = line.split(" ");
+            String fieldName = lineData[0];
+            for (int i = 1; i < lineData.length; i++)
+                indexer.add(new Field(fieldName, lineData[i]));
+            lineNum++;
+        }
+        
+        // Check that all of our data is in the database
+        for (int i = 0; i < lines.length; i++)
+        {
+            String[] lineData = lines[i].split(" ");
+            String fieldName = lineData[0];
+            for (int k = 1; k < lineData.length; k++)
+            {
+                Field field = new Field(fieldName, lineData[k]);
+                Set<String> identifiers = database.getValue(field);
+                assertTrue(identifiers.contains(filename + " - " + i));
+            }
+        }
+        
+        // Now we'll check that field search can access all of this.
+        FieldSearch search = new FieldSearch(database);
+        
+        // Set up a bunch of queries and the expected result
+        HashMap<Field, String> searchAndResults = new HashMap<Field, String>();
+        searchAndResults.put(new Field("classes", "OOP"), "test_data.txt - 2");
+        searchAndResults.put(new Field("names",   "Tom"), "test_data.txt - 0");
+        searchAndResults.put(new Field("letters", "a"),   "test_data.txt - 3");
+        searchAndResults.put(new Field("days",    "Monday"), "test_data.txt - 1");
+        searchAndResults.put(new Field("days",    "OOP"), null);
+        
+        for (Field field : searchAndResults.keySet())
+        {
+            String[] results = search.findEquals(field);
+            if (searchAndResults.get(field) == null)
+                assertEquals(results.length, 0);
+            else
+                assertEquals(results[0], searchAndResults.get(field));
+        }
+    }
 }
