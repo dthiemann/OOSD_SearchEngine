c2e3531cc61eae74270bc405c8a05069e9425193
acc4tb <wenbin-lu@uiowa.edu>
2014-10-21 13:56:40 -0500

    try to write mock objects

--- part1/SearchEngine/src/edu/uiowa/cs2820/engine/tests/IndexerTest.java
+++ part1/SearchEngine/src/edu/uiowa/cs2820/engine/tests/IndexerTest.java
@@ -5,13 +5,64 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import edu.uiowa.cs2820.engine.Indexer;
+import edu.uiowa.cs2820.engine.Database;
+import edu.uiowa.cs2820.engine.Field;
+
+// mock Field class
+class FakeField extends Field {
+	public FakeField(String fieldName, String value) {
+		super(fieldName, value);
+		this.fieldName = fieldName;
+		this.value = value;
+		// TODO Auto-generated constructor stub
+	}
+
+	private String fieldName;
+	private String value;
+
+	@Override
+	public String getFieldName() {
+		return fieldName;
+	}
+	
+	@Override
+	public String getValue() {
+		return value;
+	}
+}
+
+// mock Database class
+class FakeDatabase extends Database {
+	private HashMap<Field, Integer> map;
+	
+	public FakeDatabase() {
+		map = new HashMap<Field, Integer>();
+	}
+	
+	@Override
+	public void store(Field f, String identifier) {
+		if (!map.containsKey(f))
+			map.put(f,1);
+		map.put(f, map.get(f)+1);
+	}
+	
+	@Override
+	public int getDatabaseSize() {
+		return map.keySet().size();
+	}
+}
+
 
 public class IndexerTest {
+	private FakeDatabase db;
 
     @Before
     public void setUp() throws Exception
     {
+    	db = new FakeDatabase();
     }
 
     @Test
@@ -22,7 +73,14 @@ public class IndexerTest {
          * Append an existing field
          * Try store to a closed database
          */
-        fail("Not yet implemented");
+    	Indexer I = new Indexer(db, "FakeIdentifier");
+    	assertEquals(db.getDatabaseSize(), 0);
+    	Field F1 = new Field("Name1", "233");
+    	I.add(F1);
+    	assertEquals(db.getDatabaseSize(), 1);
+    	Field F2 = new Field("Name1", "466");
+    	I.add(F2);
+    	assertEquals(db.getDatabaseSize(), 1);
     }
 
 }
