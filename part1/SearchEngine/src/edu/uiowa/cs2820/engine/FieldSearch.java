6ba9dadbb3c3f4dfbef2d0bce222ab8b769dc253
Nick Evans <nicholas-evans@uiowa.edu>
2014-10-22 20:29:50 -0500

    Add logic for searching

--- part1/SearchEngine/src/edu/uiowa/cs2820/engine/FieldSearch.java
+++ part1/SearchEngine/src/edu/uiowa/cs2820/engine/FieldSearch.java
@@ -1,22 +1,29 @@
 package edu.uiowa.cs2820.engine;
 
+import java.util.HashSet;
+
 import edu.uiowa.cs2820.engine.Field;
 import edu.uiowa.cs2820.engine.Database;
 
-public class FieldSearch
-{
+public class FieldSearch {
 
-	private String[] result = null;
 	private Database db;
+	private HashSet<String> found;
+	private String[] result;
 	
     public FieldSearch(Database database) {
     	this.db = database;
     	
     }
     
-    // Search the database for fields that match the search terms
     public String[] findEquals(Field search) {
+    	found = db.get(search);
+    	result = new String[db.getDatabaseSize()];
     	
+    	int elem = 0;
+    	for (String str: found) {
+    		result[elem] = str;
+    	}
     	
         return result;
     }
