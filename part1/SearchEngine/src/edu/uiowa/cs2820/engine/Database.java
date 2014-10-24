3b39599ddaef7533011e648575e3471e82d5f438
TomWerner <tomwer3@gmail.com>
2014-10-23 22:51:23 -0500

    More merging

--- part1/SearchEngine/src/edu/uiowa/cs2820/engine/Database.java
+++ part1/SearchEngine/src/edu/uiowa/cs2820/engine/Database.java
@@ -1,67 +1,50 @@
 package edu.uiowa.cs2820.engine;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-// There is a definitely a better way to implement this
-// I just don't know what it is yet
+import edu.uiowa.cs2820.engine.Field;
 
-// Would a singleton make more sense? That way we know there is only
-// one instance of a database ever. I'm not sure if thats considered
-// bad practice though. I think I read something about it not being
-// ideal for TDD - Tom
+/*
+ * Database used by the Indexer and FieldSearch classes for storing
+ * and retrieving indexed data.
+ * 
+ * author: Dylan Thiemann
+ * date: 10/23/14
+ * 
+ */
 
-// That's what I was thinking at first... but wouldn't we need a database instance 
-// for each index? - Dylan
 
-// I don't think so. If each index just did something like 
-//      Database.getInstance().setValueforKey(field)
-// then they'd all end up in the same database, provided we were in the same session.
+public class Database {
 
-// So it seems like that each field is going to have multiple values (according
-// to the UserDemo.java). So I think it would be best to use an ArrayList for the
-// values of each key so we can store all the values. - Dylan
-
-// Maybe I'll create a hashmap of hashmaps and use the index to access 
-// the first layer of hashmaps and then use the field for taht index to 
-// get at the rest of the data (then we could use a singleton) - Dylan
-
-// Guys, the professor said that this class "should not use any files" on his
-// webpage. Besides, what I understand is that it is the Indexer.close()'s
-// responsibility to pass fieldName, identifier and the list of values to the
-// Database class. You can see my code for details, I mocked a D.store(...) method.
-// Maybe we should discuss this in the Github's Issue section.
-// - Wenbin
-
-// Yeah we were never planning on use files. We were just trying to figure
-// out the best way to utilize the class. I posted an issue about it on the
-// Github page. - Dylan
-
-public class Database
-{
-
-    private HashMap<Field, Set<String>> map;
-
-    public Database()
-    {
-        map = new HashMap<Field, Set<String>>();
+    
+    private HashMap<Field, HashSet<String>> map;
+    private int count;
+    
+    public Database() {
+        map = new HashMap<Field, HashSet<String>>();
+        this.count = 0;
     }
-
-    public Set<String> getValue(Field field)
-    {
-        return map.get(field);
+    
+    public HashSet<String> get(Field field) {
+        if (map.containsKey(field))
+            return map.get(field);
+        else
+            return null;
     }
-
-    public void store(Field field, String identifier)
-    {
-        if (!map.containsKey(field))
-            map.put(field, new HashSet<String>());
-        map.get(field).add(identifier);
+    
+    public void store(Field field, String fileName) {
+        if (map.containsKey(field) && fileName != null) {
+            map.get(field).add(fileName);
+            count++;
+        } else if (fileName != null) {
+            HashSet<String> newSet = new HashSet<String>();
+            newSet.add(fileName);
+            map.put(field, newSet);
+            count++;
+        }
     }
-
-    public int getDatabaseSize()
-    {
-        return map.keySet().size();
+    
+    public int getDatabaseSize() {
+        return count;
     }
 }
